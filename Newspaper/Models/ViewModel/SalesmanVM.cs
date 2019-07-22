8be4cb2c4c50ac6b1a9b06ecbb923e46fe01465f
dibugsoft2019 <dibugsoft2019@outlook.com>
2019-07-22 18:11:23 +0545

    files

---
+++ Newspaper/Models/ViewModel/SalesmanVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Newspaper.Models.ViewModel
+{
+    public class SalesmanVM
+    {
+        public SalesMan salesman { get; set; }
+        public Customer customer { get; set; }
+        public string CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        
+    }
+}
